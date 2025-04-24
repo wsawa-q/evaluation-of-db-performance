4d544c87a8f69e6f78f0edbd51cee5a404c75eac
Valeriia Kurinna <kurinnaya.valeria@gmail.com>
2025-04-24 22:41:49 +0200

    jdbc impl

---
+++ evaluation/microservice-mysql-jdbc/src/main/java/cz/cuni/mff/java/kurinna/microservice/controller/QueryController.java
@@ -0,0 +1,38 @@
+package cz.cuni.mff.java.kurinna.microservice.controller;
+
+import cz.cuni.mff.java.kurinna.microservice.dto.PricingSummary;
+import cz.cuni.mff.java.kurinna.microservice.service.QueryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class QueryController {
+    private final QueryService queryService;
+
+    public QueryController(QueryService queryService) {
+        this.queryService = queryService;
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping("/q1")
+    public ResponseEntity<Map<String, Object>> getPricingSummary() {
+        int randomNumber = (int) (Math.random() * 60) + 60;
+        long startTime = System.currentTimeMillis();
+        List<PricingSummary> pricingSummary = queryService.getPricingSummary(randomNumber);
+        long endTime = System.currentTimeMillis();
+        Map<String, Object> response = new HashMap<>();
+        response.put("executionTime", (endTime - startTime) + " ms");
+        response.put("randomNumber", randomNumber);
+        response.put("pricingSummary", pricingSummary);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ evaluation/microservice-mysql-jdbc/src/main/java/cz/cuni/mff/java/kurinna/microservice/repository/UniversalRepository.java
@@ -0,0 +1,65 @@
+package cz.cuni.mff.java.kurinna.microservice.repository;
+
+import cz.cuni.mff.java.kurinna.microservice.dto.PricingSummary;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class UniversalRepository {
+
+    private final NamedParameterJdbcTemplate jdbc;
+
+    public UniversalRepository(NamedParameterJdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public List<PricingSummary> fetchPricingSummary(int days) {
+        String sql = """
+            SELECT
+              l_returnflag,
+              l_linestatus,
+              SUM(l_quantity)                     AS sum_qty,
+              SUM(l_extendedprice)                AS sum_base_price,
+              SUM(l_extendedprice * (1 - l_discount))             AS sum_disc_price,
+              SUM(l_extendedprice * (1 - l_discount) * (1 + l_tax)) AS sum_charge,
+              AVG(l_quantity)                     AS avg_qty,
+              AVG(l_extendedprice)                AS avg_price,
+              AVG(l_discount)                     AS avg_disc,
+              COUNT(*)                            AS count_order
+            FROM lineitem
+            WHERE l_shipdate <=
+              DATE_SUB('1998-12-01', INTERVAL :days DAY)
+            GROUP BY l_returnflag, l_linestatus
+            ORDER BY l_returnflag, l_linestatus
+            """;
+
+        var params = new MapSqlParameterSource()
+                .addValue("days", days);
+
+        return jdbc.query(sql, params, pricingSummaryMapper);
+    }
+
+    private final RowMapper<PricingSummary> pricingSummaryMapper = new RowMapper<>() {
+        @Override
+        public PricingSummary mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new PricingSummary(
+                    rs.getString("l_returnflag"),
+                    rs.getString("l_linestatus"),
+                    rs.getDouble("sum_qty"),
+                    rs.getDouble("sum_base_price"),
+                    rs.getDouble("sum_disc_price"),
+                    rs.getDouble("sum_charge"),
+                    rs.getDouble("avg_qty"),
+                    rs.getDouble("avg_price"),
+                    rs.getDouble("avg_disc"),
+                    rs.getLong("count_order")
+            );
+        }
+    };
+}
